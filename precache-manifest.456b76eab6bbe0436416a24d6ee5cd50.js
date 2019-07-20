11fcfccc2b6a3c61c10bf2b5987f8746785eb3e1
Roman <rmnbrtshk@gmail.com>
2019-07-20 13:20:01 +0300

    Updates

---
+++ precache-manifest.456b76eab6bbe0436416a24d6ee5cd50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6ee75123c70f06909b27454bf86fe1",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "7b72db8c28c9b90a50f1",
+    "url": "/calendar/static/css/main.c66f2895.chunk.css"
+  },
+  {
+    "revision": "46bb22cdc8c393ecc5c7",
+    "url": "/calendar/static/js/2.74ab2991.chunk.js"
+  },
+  {
+    "revision": "7b72db8c28c9b90a50f1",
+    "url": "/calendar/static/js/main.6cb4ddec.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
